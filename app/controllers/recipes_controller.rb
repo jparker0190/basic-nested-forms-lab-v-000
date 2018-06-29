8f6d97c072eabb3eed8671a2ae2ebf33d0b09bf5
Joseph Parker <jparker010190@gmail.com>
2018-06-29 01:48:56 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,17 +2,17 @@ class RecipesController < ApplicationController
   def show
      @recipe = Recipe.find(params[:id])
    end
- 
+
    def index
      @recipes = Recipe.all
    end
- 
+
    def new
      @recipe = Recipe.new
      @recipe.ingredients.new
      @recipe.ingredients.new
    end
- 
+
    def create
      @recipe = Recipe.new(recipe_params)
      if @recipe.save
@@ -21,7 +21,7 @@ class RecipesController < ApplicationController
        render :new
      end
    end
- 
+
    private
    def recipe_params
      params.require(:recipe).permit(
